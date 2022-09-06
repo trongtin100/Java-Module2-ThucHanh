9ab323c771758ae7a1159f619106f6ac52300c12
Admin <trongtin100@gmail.com>
2022-09-06 14:11:02 +0700

    Bai2-KhaiBaoBien

---
+++ src/VariableDataType.java
@@ -0,0 +1,17 @@
+public class VariableDataType {
+    public static void main(String[] args) {
+        int i = 10;             // khai bao bien i co gia tri la 10;
+        float f = 20.5f;        // khai bao bien f co kieu la float co gia tri la 20.5
+        double d = 20.5;        // khai báo biến d có kiểu là double có giá trị là 20.5
+        boolean b = true;       // khai báo biến b là kiểu boolean có giá trị là true.
+        char c = 'a';           // Khai báo biến c có kiểu là char có giá trị là ký tự a.
+        String s = "Sai Gon";   // Khai báo biến s có kiểu là String có giá trị là Sai Gòn.
+        System.out.println("i = " + i );
+        System.out.println("f = " + f );
+        System.out.println("d = " + d );
+        System.out.println("b = " + b );
+        System.out.println("c = " + c );
+        System.out.println("s = " + s );
+
+    }
+}
