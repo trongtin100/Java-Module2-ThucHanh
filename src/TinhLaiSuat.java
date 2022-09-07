764ba77b54e7bafc457ecccfdceb4cd8ac5b68df
Admin <trongtin100@gmail.com>
2022-09-07 15:09:36 +0700

    boiSoChungLonNhat

--- src/TinhLaiSuat.java
+++ src/TinhLaiSuat.java
@@ -4,17 +4,18 @@ public class TinhLaiSuat {
         double money = 1.0;
         int month = 1;
         double interesRate = 1.0;
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter investment amount: ");
-        money = input.nextDouble();
+        money = scanner.nextDouble();
         System.out.println("Enter number of months:");
-        month = input.nextInt();
+        month = scanner.nextInt();
         System.out.println("Enter annual interest rate in percentage:");
-        interesRate = input.nextDouble();
+        interesRate = scanner.nextDouble();
         double totalInterest = 0;
         for ( int i=0; i < month; i++) {
             totalInterest += money*(interesRate/100)/12*month;
             System.out.println("Total of interest: " + totalInterest);
         }
+
     }
 }
---
+++ src/UocBoiSoChungLonNhat.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class UocBoiSoChungLonNhat {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner (System.in);
+        System.out.print ("So nhap vao a:");
+        int a = scanner.nextInt();
+        System.out.print ("So nhap vao b:");
+        int b = scanner.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a == 0 || b == 0) {
+            System.out.println("No greatest common factor");
+        }
+        while ( a != b ) {
+            if ( a > b )
+                a = a - b;
+            else
+                b = b - a;
+        }
+    }
+}
